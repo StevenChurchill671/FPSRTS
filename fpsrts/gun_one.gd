0fa771312f80ee1653e5a88c6e6a8e4ab8b35ec4
Churchill <churchillsteve05@gmail.com>
2026-08-06 04:31:34 +0100

    Add files via upload
    introducing bullet.

--- fpsrts/gun_one.gd
+++ fpsrts/gun_one.gd
@@ -1,12 +1,22 @@
 extends Node3D
 var ammoTotal = 300
 var ammmoRemaining = 60
+var cooldown = false
 
 func triggerPull():
-	if ammmoRemaining > 0:
+	if ammmoRemaining > 0 && !cooldown:
 		shoot();
 		ammmoRemaining -=1
+		cooldown = true
+		$Timer.start()
 func shoot():
 	$MeshInstance3D/gunflare.show()
 	$MeshInstance3D/gunflare.flare()
 	#$MeshInstance3D/gunflare.hide()
+
+func test():
+	var bullet = MeshInstance3D.new()
+	$projectile.add_child(bullet)
+	
+func _on_timer_timeout() -> void:
+	cooldown = false
