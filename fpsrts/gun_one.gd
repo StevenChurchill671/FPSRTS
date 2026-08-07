64170c442da58263b9704772b1fc188a63c4986c
Churchill <churchillsteve05@gmail.com>
2026-08-07 06:32:48 +0100

    Add files via upload
    The bullets move away from the gun up to a maximum distance, when reached they self delete.

--- fpsrts/gun_one.gd
+++ fpsrts/gun_one.gd
@@ -19,20 +19,20 @@ func shoot():
 func test():
 	var localBullet = bullet.instantiate()
 	$projectilehold.add_child(localBullet)
-	localBullet.rotate(Vector3.UP, deg_to_rad(90))
+	localBullet.rotate(Vector3.FORWARD, deg_to_rad(90))
 	#$projectilehold.global_position += Vector3(0,0.1,0)
 	#var gunLocation = $".".global_position
 	#localBullet.position += Vector3(0,0.1,0)
 	bulletDistancer(localBullet)
 func bulletDistancer(localBullet):
-	var localBulletDistance = 10
+	var localBulletDistance = 60
 	while localBulletDistance > 0:
 		waiting = true
 		$bulletdelayer.start()
-		await _on_bulletdelayer_timeout()
-		localBullet.position += Vector3(0,0.1,0)
+		await $bulletdelayer.timeout
+		localBullet.position += Vector3(0.1,0,0)
 		localBulletDistance -=1
-	#localBullet.queue_free()
+	localBullet.queue_free()
 	
 func _on_timer_timeout() -> void:
 	cooldown = false
