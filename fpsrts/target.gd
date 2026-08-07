77e856a88e4914dd84b3b78134159c62c55b9ed5
Churchill <churchillsteve05@gmail.com>
2026-08-07 07:37:05 +0100

    Add files via upload
    implemented red sqaures of death, they dont actually cause death yet, but if you hit them with 10 bullets they die, and they follow you around. And they are red.

---
+++ fpsrts/target.gd
@@ -0,0 +1,27 @@
+extends CharacterBody3D
+
+var health = 10
+
+func damaged():
+	health -= 1
+	if health < 1:
+		queue_free()
+		
+
+
+func _on_target_area_3d_area_entered(area: Area3D) -> void:
+	if area.has_meta("bulletArea"):
+		damaged()
+
+var speed := 2.0
+@onready var agent: NavigationAgent3D = $NavigationAgent3D
+func _ready():
+	agent.path_desired_distance = 0.5
+	agent.target_desired_distance = 0.5
+func set_target():
+	await get_tree().physics_frame
+	agent.set_target_position(get_parent().get_parent().get_child(0).global_position)
+func _physics_process(delta):
+	set_target()
+	velocity = global_position.direction_to(agent.target_position) * speed
+	move_and_slide()
